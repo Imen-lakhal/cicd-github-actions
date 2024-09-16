14dfa5dee0029e5ce97f3d1ff70917f5ddae721b
Imen lakhal <imen.lakhal@ensi-uma.tn>
2024-09-16 12:10:33 +0100

    avec test

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const calculate = require('./calculate')
 
 
-
+app.get('/', (req, res) => {
+  res.send("Sum = "+calculate.sum(1,3));
+})
 
 app.get('/auth', (req, res) => {
   res.send('user logged in')
